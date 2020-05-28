347923fb1b626f72cd1a1a4b7397b0033b8806e0
Luxos <47970987+Luxosaeternam@users.noreply.github.com>
2020-05-28 15:02:13 +0200

    SphereCube modifications

--- src/SphereCubeBufferGeometry.js
+++ src/SphereCubeBufferGeometry.js
@@ -19,9 +19,9 @@ function SphereCubeBufferGeometry( radius, widthSegments, heightSegments, phiSta
 		thetaLength: thetaLength
 	};
 
-	// generate cube
+	var scope = this;
 
-	var tmpCube = THREE.BoxBufferGeometry(1, 1, 1, widthSegments, heightSegments, heightSegments);
+	// generate cube
 
 	radius = radius || 1;
 
@@ -37,12 +37,22 @@ function SphereCubeBufferGeometry( radius, widthSegments, heightSegments, phiSta
 	var thetaEnd = thetaStart + thetaLength;
 
 	var ix, iy;
+	var depth = 1;
+	var height = 1;
+	var width = 1;
+	var depthSegments = heightSegments;
 
 	var index = 0;
 	var grid = [];
 
-	var vertex = new Vector3();
-	var normal = new Vector3();
+	var vertex = new THREE.Vector3();
+	var normal = new THREE.Vector3();
+
+	var numberOfVertices = 0;
+	var groupStart = 0;
+
+	var u = 1;
+	var v = 1;
 
 	// buffers
 
@@ -51,9 +61,18 @@ function SphereCubeBufferGeometry( radius, widthSegments, heightSegments, phiSta
 	var normals = [];
 	var uvs = [];
 
+	buildPlane( 'z', 'y', 'x', - 1, - 1, depth, height, width, depthSegments, heightSegments, 0 ); // px
+	buildPlane( 'z', 'y', 'x', 1, - 1, depth, height, - width, depthSegments, heightSegments, 1 ); // nx
+	buildPlane( 'x', 'z', 'y', 1, 1, width, depth, height, widthSegments, depthSegments, 2 ); // py
+	buildPlane( 'x', 'z', 'y', 1, - 1, width, depth, - height, widthSegments, depthSegments, 3 ); // ny
+	buildPlane( 'x', 'y', 'z', 1, - 1, width, height, depth, widthSegments, heightSegments, 4 ); // pz
+	buildPlane( 'x', 'y', 'z', - 1, - 1, width, height, - depth, widthSegments, heightSegments, 5 ); // nz
+
 	// generate vertices, normals and uvs
 
-	for (vIndex = 0; vIndex < tmpCube.vertices.lenght; v += 3) {
+	for (var vIndex = 0; vIndex < vertices.length; v += 3) {
+
+		console.log(vIndex);
 
 		vertex.x = vertices[vIndex];
 		vertex.y = vertices[vIndex + 1];
@@ -73,7 +92,7 @@ function SphereCubeBufferGeometry( radius, widthSegments, heightSegments, phiSta
 
 		uvs.push( u, 1 - v );
 
-		verticesRow.push( index ++ );
+		//verticesRow.push( index ++ );
 	}
 
 	/*
@@ -147,6 +166,110 @@ function SphereCubeBufferGeometry( radius, widthSegments, heightSegments, phiSta
 	this.addAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
 	this.addAttribute( 'normal', new Float32BufferAttribute( normals, 3 ) );
 	this.addAttribute( 'uv', new Float32BufferAttribute( uvs, 2 ) );
+
+	function buildPlane( u, v, w, udir, vdir, width, height, depth, gridX, gridY, materialIndex ) {
+
+		var segmentWidth = width / gridX;
+		var segmentHeight = height / gridY;
+
+		var widthHalf = width / 2;
+		var heightHalf = height / 2;
+		var depthHalf = depth / 2;
+
+		var gridX1 = gridX + 1;
+		var gridY1 = gridY + 1;
+
+		var vertexCounter = 0;
+		var groupCount = 0;
+
+		var ix, iy;
+
+		var vector = new THREE.Vector3();
+
+		// generate vertices, normals and uvs
+
+		for ( iy = 0; iy < gridY1; iy ++ ) {
+
+			var y = iy * segmentHeight - heightHalf;
+
+			for ( ix = 0; ix < gridX1; ix ++ ) {
+
+				var x = ix * segmentWidth - widthHalf;
+
+				// set values to correct vector component
+
+				vector[ u ] = x * udir;
+				vector[ v ] = y * vdir;
+				vector[ w ] = depthHalf;
+
+				// now apply vector to vertex buffer
+
+				vertices.push( vector.x, vector.y, vector.z );
+
+				// set values to correct vector component
+
+				vector[ u ] = 0;
+				vector[ v ] = 0;
+				vector[ w ] = depth > 0 ? 1 : - 1;
+
+				// now apply vector to normal buffer
+
+				normals.push( vector.x, vector.y, vector.z );
+
+				// uvs
+
+				uvs.push( ix / gridX );
+				uvs.push( 1 - ( iy / gridY ) );
+
+				// counters
+
+				vertexCounter += 1;
+
+			}
+
+		}
+
+		// indices
+
+		// 1. you need three indices to draw a single face
+		// 2. a single segment consists of two faces
+		// 3. so we need to generate six (2*3) indices per segment
+
+		for ( iy = 0; iy < gridY; iy ++ ) {
+
+			for ( ix = 0; ix < gridX; ix ++ ) {
+
+				var a = numberOfVertices + ix + gridX1 * iy;
+				var b = numberOfVertices + ix + gridX1 * ( iy + 1 );
+				var c = numberOfVertices + ( ix + 1 ) + gridX1 * ( iy + 1 );
+				var d = numberOfVertices + ( ix + 1 ) + gridX1 * iy;
+
+				// faces
+
+				indices.push( a, b, d );
+				indices.push( b, c, d );
+
+				// increase counter
+
+				groupCount += 6;
+
+			}
+
+		}
+
+		// add a group to the geometry. this will ensure multi material support
+
+		scope.addGroup( groupStart, groupCount, materialIndex );
+
+		// calculate new start value for groups
+
+		groupStart += groupCount;
+
+		// update total number of vertices
+
+		numberOfVertices += vertexCounter;
+
+	}
 
 }
 
